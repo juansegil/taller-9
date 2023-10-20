50852a148534fb8caa63d51e1596bf22d9d3406a
juansegil <141371260+juansegil@users.noreply.github.com>
2023-10-20 15:41:58 -0500

    Update app.py

--- taller 9/DatosMeteorologicos/app.py
+++ taller 9/DatosMeteorologicos/app.py
@@ -1,6 +1,10 @@
 from DatosMeteorologicos import DatosMeteorologicos
 
-
 datos_meteorologicos = DatosMeteorologicos('datos.txt')
 resultados = datos_meteorologicos.procesar_datos()
 
+print(f"Temperatura promedio: {resultados[0]}")
+print(f"Humedad promedio: {resultados[1]}")
+print(f"Presión promedio: {resultados[2]}")
+print(f"Velocidad promedio del viento: {resultados[3]}")
+print(f"Dirección predominante del viento: {resultados[4]}")
